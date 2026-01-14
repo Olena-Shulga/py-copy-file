47604bd944b51c6984111edc6205f667fc664015
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-01-14 15:32:21 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        print("The command is not formatted correctly")
+    else:
+        file1 = command.split(" ")[1]
+        file1_copy = command.split(" ")[2]
+        if file1 != file1_copy and command.split(" ")[0] == "cp":
+            try:
+                with (open(file1, "r") as file_in,
+                      open(file1_copy, "w") as file_out):
+                    file_out.write(file_in.read())
+            except FileNotFoundError:
+                print(f"The file {file1} does not exist")
